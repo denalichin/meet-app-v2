674261e8f1e4183f9766eff6bedb354fd90f291f
denalichin <denalichin@gmail.com>
2022-03-27 23:46:52 -0700

    implemented React.memo for child component functions so they no longer rerender everytime the parent state changes

--- frontend/src/components/TimeSelector.js
+++ frontend/src/components/TimeSelector.js
@@ -1,8 +1,7 @@
 import React, {Component, useState} from "react";
 
 
-export function TimeSelector({
-    setTimeFunction, htmlName, htmlId, defaultValue,}) {
+const TimeSelector = React.memo( function TimeSelector({setTimeFunction, htmlName, htmlId, defaultValue}) {
 
     // const [selectedValue, setSelectedValue] = useState(defaultValue);
 
@@ -35,86 +34,29 @@ export function TimeSelector({
         {text: "12:00  AM"}
     ]
 
-    // console.log(htmlName);
-
     const assignValue=(event)=>{
-        // setSelectedValue(event.target.value);
         setTimeFunction(event.target.value);
         console.log("selected", times[event.target.value].text, "for", htmlName);
     }
 
     return(
         <select name={htmlName} id={htmlId} 
-        onChange={(event) => assignValue(event)}
-        >
+        onChange={(event) => assignValue(event)}>
             {/* Mapping through each object in our time array
           and returning an option element with the appropriate attributes / values.
          */}
             {times.map((t, index) => {
                 if(index == defaultValue){
+                    console.log("RENDERING TIME SELECTOR");
                     return <option value={index} selected="selected">{t.text}</option>
                 } else {
                     return <option value={index}>{t.text}</option>
                 }
-                    
-
-                
             })
             }
         </select>
     )
 }
+);
 
-// export class TimeSelector extends Component {
-
-//     constructor (props){
-//         super(props);
-//             this.state = {
-//                 test: "default"
-//             };
-//     }
-
-//     updateState = (param) => {
-//         console.log("setting param to ", param);
-//         this.setState({test: param});
-//     }
-
-//     render(){
-
-//         console.log("test: ", this.state.test);
-//         return(
-//         <div>
-//         <h1> TIME SELECTOR</h1>
-        // <select>
-        //     <option>12:00  AM</option>  
-        //     <option>1:00  AM</option>  
-        //     <option>2:00  AM</option>  
-        //     <option>3:00  AM</option>  
-        //     <option>4:00  AM</option>  
-        //     <option>5:00  AM</option> 
-        //     <option>6:00  AM</option> 
-        //     <option>7:00  AM</option> 
-        //     <option>8:00  AM</option> 
-        //     <option selected="selected">9:00  AM</option>
-        //     <option>10:00  AM</option> 
-        //     <option>11:00  AM</option> 
-        //     <option>12:00  PM</option> 
-        //     <option>1:00  PM</option> 
-        //     <option>2:00  PM</option> 
-        //     <option>3:00  PM</option> 
-        //     <option>4:00  PM</option> 
-        //     <option>5:00  PM</option> 
-        //     <option>6:00  PM</option>  
-        //     <option>7:00  PM</option> 
-        //     <option>8:00  PM</option> 
-        //     <option>9:00  PM</option> 
-        //     <option>10:00  PM</option> 
-        //     <option>11:00  PM</option>
-        //     <option>12:00  AM</option>
-        // </select>
-
-//         </div>
-           
-//         );
-//     }
-// }
+export {TimeSelector};
